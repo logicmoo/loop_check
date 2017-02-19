f9fa78f5e58b33b7d9af92301f6d094284936174
root <logicmoo@gmail.com>
2017-02-18 22:57:40 -0800

    removed unused txt file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # loop_check
-A new simple loop_check 
+New simple loop checking
+
+Allows code to declare special locations that loop prevention will occur
+
+
---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(loop_check).
+version('0.0.1').
+title('New simple loop checking').
+keywords([source,goal_expansion]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+maintainer( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+home( 'https://github.com/TeamSPoon/loop_check' ).
+download( 'https://github.com/TeamSPoon/loop_check/release/*.zip' ).
+
