008ab31271d24af2b5a5a982802865b045da67d1
root <logicmoo@gmail.com>
2017-02-23 09:24:58 -0800

    v0.0.8

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(loop_check).
-version('0.0.7').
+version('0.0.8').
 title('New simple loop checking').
 keywords([source,goal_expansion]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
