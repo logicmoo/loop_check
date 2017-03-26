3cfef41a1383a64a68b7733f628116fb986c2e1d
root <logicmoo@gmail.com>
2017-03-26 05:43:38 -0700

    v1.1.117

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(loop_check).
-version('1.1.115').
+version('1.1.117').
 title('New simple loop checking').
 keywords([source,goal_expansion]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
